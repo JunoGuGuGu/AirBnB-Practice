c48c11a8bceb6e4df8a38a2380405b2e2fdb1b49
Juno Gu <juno.gu@kellogg.northwestern.edu>
2024-04-12 03:52:16 +0000

    0411 update

--- practice-6.sql
+++ practice-6.sql
@@ -15,4 +15,5 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
-
+SELECT listings.neighborhood, reviews.id, reviews.listing_id, reviews.comments FROM reviews
+INNER JOIN id ON listings.id = reviews.listing_id;
